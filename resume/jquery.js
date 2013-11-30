6a5f9f6cc892a86e95d4990ae196bc647ce4354b
berma <adamscberman@gmail.com>
2013-11-30 15:35:04 -0600

    jquery??

--- resume/jquery.js
+++ resume/jquery.js
@@ -1,6 +1,29 @@
 $(document).ready(function () {
 	$("#graphics").click(function () {
-		$('#graphicst').show(200);
+		$('#graphicst').toggle(1000);
+	});
+
+	$("#backpacking").click(function () {
+		$('#backpackingt').toggle(1000);
+	});
+
+	$("#marketing").click(function () {
+		$('#marketingt').toggle(1000);
+	});
+
+	$("#northwestern").click(function () {
+		$('#northwesternt').toggle(1000);
+	});
+
+	$("#parks").click(function () {
+		$('#parkst').toggle(1000);
+	});
+
+	$("#printmaking").click(function () {
+		$('#printmakingt').toggle(1000);
+	});
+
+	$("#web").click(function () {
+		$('#webt').toggle(1000);
 	});
 });
-
