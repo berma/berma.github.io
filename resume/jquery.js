48c38baf45e09dd6a9ca2200c615dc1636522935
berma <adamscberman@gmail.com>
2013-11-30 15:39:16 -0600

    any effect?

--- resume/jquery.js
+++ resume/jquery.js
@@ -2,27 +2,39 @@ $(document).ready(function () {
 	$("#graphics").click(function () {
 		$('#graphicst').toggle(1000);
 	});
+	});
 
+$(document).ready(function () {
 	$("#backpacking").click(function () {
 		$('#backpackingt').toggle(1000);
 	});
+	});
 
+$(document).ready(function () {
 	$("#marketing").click(function () {
 		$('#marketingt').toggle(1000);
 	});
+	});
 
+$(document).ready(function () {
 	$("#northwestern").click(function () {
 		$('#northwesternt').toggle(1000);
 	});
+	});
 
+$(document).ready(function () {
 	$("#parks").click(function () {
 		$('#parkst').toggle(1000);
 	});
+	});
 
+$(document).ready(function () {
 	$("#printmaking").click(function () {
 		$('#printmakingt').toggle(1000);
 	});
+	});
 
+$(document).ready(function () {
 	$("#web").click(function () {
 		$('#webt').toggle(1000);
 	});
