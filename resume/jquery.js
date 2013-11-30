090378b0bbc655d44441622e2dbe5e729405bde4
berma <adamscberman@gmail.com>
2013-11-30 16:40:19 -0600

    when it worked

--- resume/jquery.js
+++ resume/jquery.js
@@ -1,45 +1,49 @@
 $(document).ready(function () {
 	$("#graphics").click(function () {
-		$(".hidden").fadeOut(400, function(){ 
+		$(".hidden").fadeOut(400);
 		$("#graphicst").fadeIn(1000);	
 	});
-  });		
-
-	$("#backpacking").click(function () {
-		$(".hidden").fadeOut(400, function(){
-		$("#backpackingt").fadeIn(1000);
 	});
 
+$(document).ready(function () {
+	$("#backpacking").click(function () {
+		$(".hidden").fadeOut(400);
+		$("#backpackingt").fadeIn(1000);
+	});
+	});
+
+$(document).ready(function () {
 	$("#marketing").click(function () {
 		$(".hidden").fadeOut(400);
 		$("#marketingt").fadeIn(1000);	
 	});
+	});
 
-
+$(document).ready(function () {
 	$("#northwestern").click(function () {
 		$(".hidden").fadeOut(400);
 		$("#northwesternt").fadeIn(1000);	
 	});
+	});
 
+$(document).ready(function () {
 	$("#parks").click(function () {
 		$(".hidden").fadeOut(400);
 		$("#parkst").fadeIn(1000);	
 	});
+	});
 
-
+$(document).ready(function () {
 	$("#printmaking").click(function () {
 		$(".hidden").fadeOut(400);
 		$("#printmakingt").fadeIn(1000);	
 	});
+	});
 
-
+$(document).ready(function () {
 	$("#web").click(function () {
 		$(".hidden").fadeOut(400);
 		$("#webt").fadeIn(1000);	
 	});
-
-	$("#gardening").click(function () {
-		$(".hidden").fadeOut(400);
-		$("#gardeningt").fadeIn(1000);	
-	});	
+	
 });
