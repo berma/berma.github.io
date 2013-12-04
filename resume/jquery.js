9c8c8b8e24a1e1923c93d624238397ffa2d0d767
berma <adamscberman@gmail.com>
2013-12-04 13:54:25 -0600

    the fader

--- resume/jquery.js
+++ resume/jquery.js
@@ -3,67 +3,67 @@ $(document).ready(function () {
 		$(".hidden").fadeOut("slow");
 		$("#skillst").fadeIn("slow");
 	});
-	});
+});
 
 $(document).ready(function () {
 	$("#graphics").click(function () {
 		$(".hidden").fadeOut("slow");
 		$("#graphicst").fadeIn("slow");
 	});
-	});
+});
 
 $(document).ready(function () {
 	$("#backpacking").click(function () {
 		$(".hidden").fadeOut("slow");
 		$("#backpackingt").fadeIn("slow");
 	});
-	});
+});
 
 $(document).ready(function () {
 	$("#marketing").click(function () {
 		$(".hidden").fadeOut("slow");
 		$("#marketingt").fadeIn("slow");	
 	});
-	});
+});
 
 $(document).ready(function () {
 	$("#northwestern").click(function () {
 		$(".hidden").fadeOut("slow");
 		$("#northwesternt").fadeIn("slow");
 	});
-	});
+});
 
 $(document).ready(function () {
 	$("#parks").click(function () {
 		$(".hidden").fadeOut("slow");
 		$("#parkst").fadeIn("slow");	
 	});
-	});
+});
 
 $(document).ready(function () {
 	$("#printmaking").click(function () {
 		$(".hidden").fadeOut("slow");
 		$("#printmakingt").fadeIn("slow");	
 	});
-	});
+});
 
 $(document).ready(function () {
 	$("#web").click(function () {
 		$(".hidden").fadeOut("slow");
 		$("#webt").fadeIn("slow");	
 	});	
-	});
+});
 
 $(document).ready(function () {
 	$("#garden").click(function () {
 		$(".hidden").fadeOut("slow");
 		$("#gardent").fadeIn("slow");	
 	});	
-	});
+});
 
 $(document).ready(function () {
 	$("#parks").click(function () {
 		$(".hidden").fadeOut("slow");
 		$("#parkst").fadeIn("slow");	
 	});	
-	});
+});
